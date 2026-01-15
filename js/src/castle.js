f27e58f6fe07e9c449985a4025386c62b0d03871
standardloop <joshuaeli1234@gmail.com>
2026-01-15 17:59:14 +0800

    interesting

--- js/src/castle.js
+++ js/src/castle.js
@@ -38,13 +38,103 @@ export function DrawCastle(
 
   let realSidePositionX;
   for (let layersY = 1; layersY <= bricksHigh; layersY++) {
-    if (layersY % 2 === 1) {
+    if (
+      (playerNumber === PLAYER_NUMBERS.PLAYER_2 && layersY % 2 === 1) ||
+      (playerNumber === PLAYER_NUMBERS.PLAYER_1 && layersY % 2 === 0)
+    ) {
       realSidePositionX = sidePositionX - (flipper * brickWidth) / 2;
     } else {
       realSidePositionX = sidePositionX;
     }
 
     for (let layersX = 1; layersX <= castleWidth; layersX++) {
+      if (
+        (!(layersY === bricksHigh && layersX % 2 === 0) &&
+          playerNumber === PLAYER_NUMBERS.PLAYER_2) ||
+        (!(layersY === bricksHigh && layersX % 2 === 1) &&
+          playerNumber === PLAYER_NUMBERS.PLAYER_1)
+      ) {
+        drawBrick(
+          ctx,
+          realSidePositionX + layersX * brickWidth * flipper,
+          castleStartY - brickHeight * layersY,
+          brickWidth,
+          brickHeight,
+          true,
+        );
+      }
+    }
+  }
+  drawPillar(
+    canvasWidth,
+    canvasHeight,
+    ctx,
+    color,
+    bricksHigh,
+    playerNumber,
+    castleWidth,
+    true,
+  );
+  drawPillar(
+    canvasWidth,
+    canvasHeight,
+    ctx,
+    color,
+    bricksHigh,
+    playerNumber,
+    castleWidth,
+    false,
+  );
+}
+
+function drawPillar(
+  canvasWidth,
+  canvasHeight,
+  ctx,
+  color,
+  bricksHigh,
+  playerNumber,
+  castleWidth,
+  isLeft,
+) {
+  const pillarExtension = 3;
+
+  bricksHigh += pillarExtension;
+  const brickWidth = getBrickWidth(canvasWidth) / 2;
+  const brickHeight = getBrickHeight(canvasHeight);
+
+  const pillarWidth = 5;
+  ctx.fillStyle = color;
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "black";
+  let flipper = 1;
+  let sidePositionX;
+  if (playerNumber === PLAYER_NUMBERS.PLAYER_1) {
+    sidePositionX = canvasWidth * 0.2;
+    if (isLeft) {
+      sidePositionX -= brickWidth * pillarWidth - brickWidth * 2 * castleWidth;
+      sidePositionX += brickWidth;
+    } else {
+      sidePositionX -= brickWidth * pillarWidth;
+      sidePositionX += brickWidth;
+    }
+  } else {
+    sidePositionX = canvasWidth * 0.8;
+    if (isLeft) {
+      sidePositionX += brickWidth * pillarWidth - brickWidth * 2 * castleWidth;
+    } else {
+      sidePositionX += brickWidth * pillarWidth;
+    }
+    flipper = -1;
+  }
+
+  let castleStartY = GetGrassStart(canvasHeight);
+
+  let realSidePositionX;
+  for (let layersY = 1; layersY <= bricksHigh; layersY++) {
+    realSidePositionX = sidePositionX;
+
+    for (let layersX = 1; layersX <= pillarWidth; layersX++) {
       if (!(layersY === bricksHigh && layersX % 2 === 0)) {
         drawBrick(
           ctx,
